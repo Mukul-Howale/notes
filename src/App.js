9b958dcd31aa4c107205d6c1cfdbab84bc27cde9
Mukul Howale <howalemukul@gmail.com>
2022-10-16 19:57:55 +0530

    Update local storage code
    Added useEffect hook which changes local storage items when changes are made to the notes state

--- src/App.js
+++ src/App.js
@@ -18,37 +18,31 @@ import "react-mde/lib/styles/css/react-mde-all.css";    // used for markdown sty
      */
 
 export default function App() {
-    const [notes, setNotes] = React.useState(() =>{
-        if(!localStorage.getItem("notes")) localStorage.setItem("notes", JSON.stringify([]))
-        return JSON.parse(localStorage.getItem("notes"))
-    })
+    const [notes, setNotes] = React.useState(JSON.parse(localStorage.getItem("notes")) || [])
 
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
     )
+
+    React.useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes))
+    }, [notes])
 
     function createNewNote() {
         const newNote = {
             id: nanoid(),
             body: "# Type your markdown note's title here"
         }
-        setNotes(oldNotes => {
-            localStorage.setItem("notes", JSON.stringify([newNote, ...oldNotes]))
-            return [newNote, ...oldNotes]
-        })
+        setNotes(oldNotes => [newNote, ...oldNotes])
         setCurrentNoteId(newNote.id)
     }
 
     function updateNote(text) {
-        setNotes(oldNotes => {
-            const updatedNote = oldNotes.map(oldNote => {
-                return oldNote.id === currentNoteId
-                    ? { ...oldNote, body: text }
-                    : oldNote
-            })
-            localStorage.setItem("notes",JSON.stringify([...updatedNote]));
-            return updatedNote;
-        })
+        setNotes(oldNotes => oldNotes.map(oldNote => {
+            return oldNote.id === currentNoteId
+                ? { ...oldNote, body: text }
+                : oldNote
+        }))
     }
 
     function findCurrentNote() {
